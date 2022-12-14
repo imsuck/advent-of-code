24d39380e156b1a49ff26e9283c0b97375036001
imsuck <imsuck12@gmail.com>
2022-12-14 13:03:33 +0700

    day 5: In progress...

---
+++ yr2022/src/bin/day5/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = std::fs::read_to_string(yr2022::get_input()).expect("Can't read input D:");
+
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(_input: &str) {
+    // println!("Part one: {}");
+}
+
+fn part_two(_input: &str) {
+    // println!("Part two: {}");
+}
